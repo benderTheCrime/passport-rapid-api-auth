01e93897feb28061ee4ea711f241825a91aa3a94
benderTheCrime <joe.groseclose@gmail.com>
2019-10-27 13:35:16 -0400

    test: add failure test cases

--- index.test.js
+++ index.test.js
@@ -17,13 +17,31 @@ describe('RapidAPIPassportStrategy', function() {
       this.strategy.success = sinon.stub()
       this.strategy.fail = sinon.stub()
     })
-    it('test auth strategy success', function() {
+    it('checks proxy secret and calls passport success', function() {
       this.strategy.authenticate({headers: this.headers})
       chai.assert(this.strategy.success.calledOnceWith({
         subscription: 'FREE',
         name: 'test user',
         version: '1.1.1',
       }, {}))
+      chai.assert(this.strategy.fail.notCalled)
+    })
+    it('checks missing proxy secret and calls passport fail', function() {
+      this.strategy.authenticate({headers: {}})
+      chai.assert(this.strategy.success.notCalled)
+      chai.assert(this.strategy.fail.calledOnceWith({
+        errors: ['No RapidAPI Proxy Secret provided.'],
+        result: null,
+      }, 401))
+    })
+    it('checks incorrect proxy secret and calls passport fail', function() {
+      this.headers['X-RapidAPI-Proxy-Secret'] = 'racecar'
+      this.strategy.authenticate({headers: this.headers})
+      chai.assert(this.strategy.success.notCalled)
+      chai.assert(this.strategy.fail.calledOnceWith({
+        errors: ['RapidAPI Proxy Secret is invalid.'],
+        result: null,
+      }, 401))
     })
   })
 })
